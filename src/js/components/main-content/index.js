a48490a3fcb72e9bdcda5567f7e9af91008bedc1
Oleg Tsyro <otsyro@spbtv.com>
2016-08-11 14:09:14 +0300

    moved component header on main-content

--- src/js/components/main-content/index.js
+++ src/js/components/main-content/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../main-content__header';
+import Header from './header';
 
 export default class MainContent extends React.Component {
   constructor(props) {
